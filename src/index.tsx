f20b065460dc0295a4a37f75fce3997697bc630d
girusta <girusta@fcecon.unr.edu.ar>
2018-12-17 21:21:34 -0300

    Recovery Pass Form done!

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import DownloadButton from './components/download-button/download-button';
 import CheckboxFilter from './components/checkbox-filter/checkbox-filter';
 import LoginForm from './components/login-form/login-form';
+import PassRecoveryForm from './components/pass-recovery-form/pass-recovery-form';
 
 export {
   DownloadButton,
   CheckboxFilter,
-  LoginForm
+  LoginForm,
+  PassRecoveryForm
 };

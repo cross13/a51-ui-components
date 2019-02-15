9899d19651d3385d9df6ea2c0ddb407b508345c5
lucas Borella <blackbox.software@gmail.com>
2019-02-15 07:53:33 -0300

    Publish  1.0.9 - item list

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import DownloadButton from './components/download-button/download-button';
 import CheckboxFilter from './components/checkbox-filter/checkbox-filter';
 import LoginForm from './components/login-form/login-form';
 import PassRecoveryForm from './components/pass-recovery-form/pass-recovery-form';
+import ItemsList from './components/items-list/items-list';
 
 export {
   DownloadButton,
   CheckboxFilter,
   LoginForm,
-  PassRecoveryForm
+  PassRecoveryForm,
+  ItemsList,
 };
